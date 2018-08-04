8fa9f1371126bbffbffa2eafe0fb5769b0cc6fad
Josh Cesana <joshuacesana@gmail.com>
2018-08-04 00:45:47 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -24,8 +24,8 @@ class PigLatinizer
     
     if vowels.include?(word[0].downcase)
       word + 'way'
-    elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
-    word[2..-1] + word[0..1] + 'ay'
+    elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
+    word[3..-1] + word[0..2] + 'ay'
     elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
       word[2..-1] + word[0..1] + 'ay'
     elsif consonants.include?(word[0].downcase)
