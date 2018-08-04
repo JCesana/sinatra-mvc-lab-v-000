e95366e88ef8fba2d846cff259febffc6d4f1284
Josh Cesana <joshuacesana@gmail.com>
2018-08-04 00:40:03 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -23,11 +23,11 @@ class PigLatinizer
     consonants = alphabet - vowels
     
     if vowels.include?(word[0])
-      word + 'ay'
+      word + 'way'
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
-      word[2..-1] + word[0..1] + 'ay'
+      word[2..-1] + word[0..1] + 'way'
     elsif consonants.include?(word[0])
-      word[1..-1] + word[0] + 'ay'
+      word[1..-1] + word[0] + 'way'
     else
       word
     end
