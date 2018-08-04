d50d10e2868869830a29e8837757a4a426624e7a
Josh Cesana <joshuacesana@gmail.com>
2018-08-04 00:40:38 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -25,9 +25,9 @@ class PigLatinizer
     if vowels.include?(word[0])
       word + 'way'
     elsif consonants.include?(word[0]) && consonants.include?(word[1])
-      word[2..-1] + word[0..1] + 'way'
+      word[2..-1] + word[0..1] + 'ay'
     elsif consonants.include?(word[0])
-      word[1..-1] + word[0] + 'way'
+      word[1..-1] + word[0] + 'ay'
     else
       word
     end
