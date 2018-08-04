7b289ac2c46076f1336365d549316202866fa2d4
Josh Cesana <joshuacesana@gmail.com>
2018-08-04 00:32:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end 
   
   post '/' do 
-    @analyzed_text = Piglatinizer.new(params[:user_text])
+    @analyzed_text = PigLatinizer.new(params[:user_text])
     
     erb :results
   end 
