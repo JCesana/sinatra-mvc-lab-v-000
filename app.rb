a0af5435f1dda413ebbe434ba03a1b08c06a1bf0
Josh Cesana <joshuacesana@gmail.com>
2018-08-04 00:02:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/' do 
+    @analyzed_text = PigLatinizer.new(params[:user_text])
+    
+    erb :results
+  end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,11 @@
+class PigLatinizer
+  attr_accessor :user_text 
+  
+  def initialize(text=nil)
+    @user_text = text.strip
+  end 
+  
+  def piglatinize(str)
+    
+  end 
+end 
